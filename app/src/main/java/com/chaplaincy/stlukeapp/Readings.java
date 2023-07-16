084b31e06b20d1be47ecc91705af452a77369713
nm1999 <matianyanzi@gmail.com>
2023-07-16 14:00:52 -0700

    added network state checks

--- app/src/main/java/com/chaplaincy/stlukeapp/Readings.java
+++ app/src/main/java/com/chaplaincy/stlukeapp/Readings.java
@@ -1,8 +1,13 @@
 package com.chaplaincy.stlukeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebResourceError;
@@ -14,14 +19,27 @@ import android.widget.LinearLayout;
 
 
 public class Readings extends AppCompatActivity {
+    private WebView web;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_readings);
 
-        LinearLayout linearLayout = findViewById(R.id.defaultview);
+        linearLayout = findViewById(R.id.defaultview);
         ImageView back = findViewById(R.id.back);
+        swipeRefreshLayout = findViewById(R.id.swipelayout);
+        web = findViewById(R.id.web);
+
+        if (isInternetConnected()){
+            loadWebPage();
+        }else{
+            web.setVisibility(View.GONE);
+            linearLayout.setVisibility(View.VISIBLE);
+        }
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,22 +48,40 @@ public class Readings extends AppCompatActivity {
             }
         });
 
-        WebView web = findViewById(R.id.web);
-        web.loadUrl("https://bible.usccb.org/daily-bible-reading");
-        web.setWebViewClient(new WebViewClient(){
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                super.onReceivedError(view, request, error);
-                view.setVisibility(View.GONE);
-                linearLayout.setVisibility(View.VISIBLE);
-//                Toast.makeText(Readings.this, "Internet connection needed !!!", Toast.LENGTH_SHORT).show();
+            public void onRefresh() {
+                if (isInternetConnected()){
+                    loadWebPage();
+                }else{
+                    loadnoInternetPage();
+                }
             }
         });
 
-        web.getSettings().setJavaScriptEnabled(true);
+    }
 
+    private void loadnoInternetPage() {
+        web.setVisibility(View.GONE);
+        linearLayout.setVisibility(View.VISIBLE);
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    private boolean isInternetConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null){
+            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            return activeNetwork != null && activeNetwork.isConnected();
+        }
+        return false;
+    }
+
+    private void loadWebPage() {
+        web.loadUrl("https://bible.usccb.org/daily-bible-reading");
+        web.getSettings().setJavaScriptEnabled(true);
         if (web.canGoBack()){
             web.goBack();
         }
+        swipeRefreshLayout.setRefreshing(false);
     }
 }
